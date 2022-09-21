7969b86a84a7ca14d9a14bdb5d6447ccc47fd643
Nir Parickman <nirp0109@gmail.com>
2022-09-21 12:35:38 +0300

    add sending with smtp and change report to use smtp

--- send_mail.py
+++ send_mail.py
@@ -46,7 +46,7 @@ def send_email_using_smtp(recipients:list, subject:str, message_text:str, attach
     server.login(from_addr, 'xGR*N9fF')
 
     #     send message
-    server.sendmail(msg.as_bytes(),from_addr=from_addr, to_addrs=recipients)
+    server.sendmail(msg=msg.as_bytes(),from_addr=from_addr, to_addrs=recipients)
 
 
 
